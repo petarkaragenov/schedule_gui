ecf12ae6f2202c50cdb55dc19aeee57a6012ff0b
Petar Karagenov <petarkaragenov@gmail.com>
2021-11-09 23:23:18 +0100

    Allowed clearing Inputs and Table. Modified Layout.

--- views/main.py
+++ views/main.py
@@ -5,24 +5,38 @@ from views.new import new_appointment
 from views.delete import delete_appointment
 
 
+sg.theme("DarkBrown2")
+
+
 def main():
+    def clear_inputs(window):
+        window["-SCHEDULE-"].update(empty_table, alternating_row_color=sg.theme_background_color())
+        window["-DATE-"].update("")
+
+
     appointment_idx = None
     selected_appointment = None
 
 
-    col_left = [
+    col_left_top = [
         [
             sg.Text("Pick a Date:"), 
-            sg.In(key="-DATE-", enable_events=True),
+            sg.In(key="-DATE-", enable_events=True, size=(45, 1)),
             sg.CalendarButton("", target="-DATE-", button_color=(sg.theme_background_color(), sg.theme_background_color()), image_filename="files/calendar.png", border_width=0, default_date_m_d_y=(m, d, y), no_titlebar=False, format=("%Y-%m-%d")),
-            sg.Button("Find", size=(10, 1))
-        ],
-        [
-            sg.Table(values=empty_table, headings=["Event", "Place", "Time", "Date", "Additional"], key="-SCHEDULE-", col_widths=[8, 16, 5, 10, 20], auto_size_columns=False, enable_events=True)
+            sg.Button("Find", size=(10, 1)),
         ]
     ]
 
-    col_right = [
+    col_right_top = [[sg.Button("Find More", size=(10, 1))]]
+
+
+    col_left_bottom = [
+        [
+            sg.Table(values=empty_table, headings=["Event", "Place", "Time", "Date", "Additional"], key="-SCHEDULE-", col_widths=[8, 16, 5, 10, 20], auto_size_columns=False, enable_events=True, num_rows=8, pad=(5, 10))
+        ]
+    ]
+
+    col_right_bottom = [
         [sg.Button("New", size=(10, 1))],
         [sg.Button("Update", size=(10, 1))],
         [sg.Button("Delete", size=(10, 1))],
@@ -32,7 +46,8 @@ def main():
 
 
     layout = [
-        [sg.Col(col_left), sg.Col(col_right)]
+        [sg.Col(col_left_top), sg.VerticalSeparator(color="#240808"), sg.Col(col_right_top)],
+        [sg.Col(col_left_bottom), sg.Col(col_right_bottom)]
     ]
 
 
@@ -55,19 +70,22 @@ def main():
             else:
                 appointment_idx = [appointment[0] for appointment in res["appointments"]]
                 appointments = [appointment[1:] for appointment in res["appointments"]]
-                window["-SCHEDULE-"].update(appointments, alternating_row_color="#00468b")
+                window["-SCHEDULE-"].update(appointments, alternating_row_color="#e1bd53")
         
         elif event == "-SCHEDULE-":
-            idx_selected = values["-SCHEDULE-"][0]
-            appointment = find_appointments_by_id(appointment_idx[idx_selected])["appointments"]
-            selected_appointment = {
-                "id": appointment[0],
-                "event": appointment[1],
-                "place": appointment[2],
-                "time": appointment[3],
-                "date": appointment[4],
-                "other": appointment[5]
-            }
+            if window["-SCHEDULE-"].get() == empty_table:
+                pass
+            else:
+                idx_selected = values["-SCHEDULE-"][0]
+                appointment = find_appointments_by_id(appointment_idx[idx_selected])["appointments"]
+                selected_appointment = {
+                    "id": appointment[0],
+                    "event": appointment[1],
+                    "place": appointment[2],
+                    "time": appointment[3],
+                    "date": appointment[4],
+                    "other": appointment[5]
+                }
             
         elif event == "Update":
             if selected_appointment is None:
@@ -86,6 +104,12 @@ def main():
                         sg.popup_ok(res["msg"])
                 else:
                     pass
+
+        elif event == "Clear":
+            clear_inputs(window)
+            appointment_idx = None
+            selected_appointment = None
+
 
 
     window.close()
